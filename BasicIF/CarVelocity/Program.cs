e21cf750c45218bdb6852f38fe692e5c193b4b6b
Ricardo Bricio <bricio.ricardo@hotmail.com>
2024-05-01 11:50:52 -0300

    CarVelocity - Exercise
    exercício que exibe a velocidade no carro momentânea e calcula se o motorista foi multado ou não
    IF - ELSE IF

---
+++ BasicIF/CarVelocity/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float Vcar, Multa;
+        
+        Console.WriteLine("Qual a velocidade do carro no momento (em Km/h): ");
+        Vcar = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        if (Vcar > 80)
+        {
+            Multa = (Vcar - 80) * 5;
+            System.Console.WriteLine("============== A L E R T A !!! =================");
+            System.Console.WriteLine("Velocidade acima do limite!! Você foi Multado !!");
+            System.Console.WriteLine("Multa no valor de: R${0}", Multa);
+            System.Console.WriteLine("================================================");
+
+        }
+        else if (Vcar <= 80)
+            System.Console.WriteLine("Dentro dos padrões");
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# Logica de programação
-
-Este diretorio tem o intuito de exibir minha evolução em programação
-
-OBS: Exercicios realizados juntamente com conceitos de Git
